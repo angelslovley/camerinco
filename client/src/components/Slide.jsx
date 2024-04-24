98c64ad1054412af1305af9f499767cc48bae96b
angelslovley <angelslovley@gmail.com>
2024-04-24 10:57:42 +0530

    Implementation added

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,13 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Welcome Co-workers!!!!!
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
